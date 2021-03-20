cf851689c677a8b4cff70c29b2356f6499980e33
misha-baliyska <mihimihi1@abv.bg>
2021-03-20 14:37:47 +0200

    Testing UserController

---
+++ DreamJourney/DreamJourney.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using DreamJourney.Controllers;
+using DreamJourney.Data;
+using DreamJourney.Services;
+using DreamJourney.ViewModels.User;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DreamJourney.Tests.Controllers
+{
+    public class UserControllerTests
+    {
+        [Fact]
+        public void TestUsersLoginView()
+        {
+            var options = new DbContextOptionsBuilder<DreamJourneyDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new DreamJourneyDbContext(options);
+            context.SaveChanges();
+
+            var userService = new UsersService(context);
+            var loginVM = new LoginViewModel();
+            var controller = new UsersController(userService);
+            var result = controller.Login(loginVM) as ViewResult;
+            Assert.Equal(loginVM, result.Model);
+        }
+    }
+}
