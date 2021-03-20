2433a28cdfff4062a4adc67b2f77511f51b1d582
unknown <you@example.com>
2021-03-20 14:46:23 +0200

    Testing TripController

---
+++ DreamJourney/DreamJourney.Tests/Controllers/TripControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using DreamJourney.Controllers;
+using DreamJourney.Data;
+using DreamJourney.Services;
+using DreamJourney.ViewModels.Trip;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DreamJourney.Tests.Controllers
+{
+    public class TripControllerTests
+    {
+
+        [Fact]
+        public void TestTripsListView()
+        {
+            var options = new DbContextOptionsBuilder<DreamJourneyDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new DreamJourneyDbContext(options);
+            context.SaveChanges();
+
+            var tripService = new TripsService(context);
+
+            var tripAService = new TripApplicationsService(context);
+
+            var controller = new TripsController(tripService, tripAService);
+            var result = controller.List() as ViewResult;
+            var expected = new List<TripListViewModel>();
+            Assert.Equal(expected, result.Model);
+        }
+
+        [Fact]
+        public void TestTripsEditView()
+        {
+            var options = new DbContextOptionsBuilder<DreamJourneyDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new DreamJourneyDbContext(options);
+            context.SaveChanges();
+
+            var tripService = new TripsService(context);
+            var loginVM = new TripEditViewModel();
+
+            var tripAService = new TripApplicationsService(context);
+            var controller = new TripsController(tripService, tripAService);
+
+            var result = controller.Edit(loginVM) as ViewResult;
+            Assert.Equal(loginVM, result.Model);
+        }
+    }
+}
